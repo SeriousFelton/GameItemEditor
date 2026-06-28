72128c3ff0d08f2860d74eba2b2f2d8b8d9c8168
SeriousFelton <SeriousFelton@gmail.com>
2026-06-28 16:24:36 +0700

    Создание докер-файлов
    - Добавлен docker-compose.yml
    - Добавлен Dockerfile для API
    - Настроены порты в Program.cs
    - База данных поднимается в контейнере

--- GameItemEditor.Api/Program.cs
+++ GameItemEditor.Api/Program.cs
@@ -7,16 +7,20 @@ namespace GameItemEditor.Api
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
+            builder.WebHost.ConfigureKestrel(options =>
+            {
+                options.ListenAnyIP(5000);
+                options.ListenAnyIP(5001);
+            });
+
             builder.Services.AddAuthorization();
 
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddControllers();
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
@@ -26,6 +30,7 @@ namespace GameItemEditor.Api
             app.UseHttpsRedirection();
 
             app.UseAuthorization();
+            app.MapControllers();
 
             app.Run();
         }
